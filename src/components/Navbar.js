2f51f436fab9c05225f0f19102bee23ed53fd07a
moises gomez <moisesg526@yahoo.com>
2023-03-30 22:39:47 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "../styles/Navbar.css";
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <img src="./images/logo.png" alt="Tilesbypac logo" className="logo-img"/>
+            </div>
+            <div className="nav-links">
+                <a href="#">HOME</a>
+                <a href="#">ABOUT US</a>
+                <a href="#">GALLERY</a>
+                <a href="#">CONTACT</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;

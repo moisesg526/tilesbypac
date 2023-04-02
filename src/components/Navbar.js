9e0d6acd9609a1aa7e3185187e6d0b0fc6bbdb27
moises gomez <moisesg526@yahoo.com>
2023-04-02 00:56:56 -0400

    Added a quote button on navbar for contact also gave animation to navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
 import "../styles/Navbar.css";
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="logo">
-                <img src="./images/logo.png" alt="Tilesbypac logo" className="logo-img"/>
-            </div>
-            <div className="nav-links">
-                <a href="#">HOME</a>
-                <a href="#">ABOUT US</a>
-                <a href="#">GALLERY</a>
-                <a href="#">CONTACT</a>
-            </div>
-            <div className="quote">
-                <button className="quote-btn">Get a Qoute</button>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img
+          src="./images/logo.png"
+          alt="Tilesbypac logo"
+          className="logo-img"
+        />
+      </div>
+      <div className="nav-links">
+        <a href="#" className="quote-btn">HOME</a>
+        <a href="#" className="quote-btn">ABOUT US</a>
+        <a href="#" className="quote-btn">GALLERY</a>
+        <a href="#" className="quote-btn">CONTACT</a>
+      </div>
+      <div className="quote">
+        {/* <button className="quote-btn">Get a Qoute</button> */}
+        <a href="mailto:tilesbypac@abc.com" className="quote-btn">
+          Get a Quote
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

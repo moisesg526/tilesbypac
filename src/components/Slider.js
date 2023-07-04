caf0e37deab1e0777a0c5c3bacaf49b11aa7cf0b
moises gomez <moisesg526@yahoo.com>
2023-07-03 20:46:50 -0400

    Changed image slider style

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,29 +1,49 @@
-import SimpleImageSlider from "react-simple-image-slider";
+// import SimpleImageSlider from "react-simple-image-slider";
 import "../styles/Main.css";
 
 function Main() {
-  let images = [
-    { url: "./images/bathroomdark.jpg" },
-    { url: "./images/whitekitchen.jpg" },
-    { url: "./images/bathroommodern.jpg" },
-    { url: "./images/kitchentiles.jpg" },
-    { url: "./images/bathroomsink.jpg" },
-    { url: "./images/kitchensink.jpg" },
-  ];
+  // let images = [
+  //   { url: "./images/bathroomdark.jpg" },
+  //   { url: "./images/whitekitchen.jpg" },
+  //   { url: "./images/bathroommodern.jpg" },
+  //   { url: "./images/kitchentiles.jpg" },
+  //   { url: "./images/bathroomsink.jpg" },
+  //   { url: "./images/kitchensink.jpg" },
+  // ];
 
   return (
-    <div >
-      <div className="slider">
-        <SimpleImageSlider
-          width={600}
-          height={800}
-          images={images}
-          showBullets={true}
-          showNavs={true}
-          autoPlay={true}
-          autoPlayDelay={4}
-          style={{ margin: "0 auto"}}
-        />
+    // <div >
+    //   <div className="slider">
+    //     <SimpleImageSlider
+    //       width={600}
+    //       height={800}
+    //       images={images}
+    //       showBullets={true}
+    //       showNavs={true}
+    //       autoPlay={true}
+    //       autoPlayDelay={4}
+    //       style={{ margin: "0 auto"}}
+    //     />
+    //   </div>
+    // </div>
+    <div className="container">
+      <div className="card">
+        <img src="./images/bathroomdark.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="./images/whitekitchen.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="./images/bathroommodern.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="./images/kitchentiles.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="./images/bathroomsink.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="./images/kitchensink.jpg" alt="" />
       </div>
     </div>
   );

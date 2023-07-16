a452f369d76f3419926757a89ada3b1473a14e64
moises gomez <moisesg526@yahoo.com>
2023-07-16 00:10:33 -0400

    Added links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../images/logo.png"
+import logo from "../images/logo.png";
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img
-          src={logo}
-          alt="Tilesbypac logo"
-          className="logo-img"
-        />
+        <img src={logo} alt="Tilesbypac logo" className="logo-img" />
       </div>
       <div className="nav-links">
-        <Link to="#" className="quote-btn">HOME</Link>
-        <Link to="#" className="quote-btn">ABOUT US</Link>
-        <Link to="#" className="quote-btn">GALLERY</Link>
-        <Link to="#" className="quote-btn">CONTACT</Link>
+        <Link to="/" className="quote-btn">
+          HOME
+        </Link>
+        <CustomLink to="/about" className="quote-btn">
+          ABOUT US
+        </CustomLink>
+        <CustomLink to="/gallery" className="quote-btn">
+          GALLERY
+        </CustomLink>
+        <CustomLink to="/contact" className="quote-btn">
+          CONTACT
+        </CustomLink>
       </div>
       <div className="quote">
         <a href="mailto:tilesbypac@abc.com" className="quote-btn">
@@ -23,6 +27,17 @@ function Navbar() {
         </a>
       </div>
     </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const path = window.location.pathname;
+  return (
+    <li className={path === to ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
 

058a2f83664952ca4f172e4800aafe4fb80828f0
moises gomez <moisesg526@yahoo.com>
2023-07-18 00:00:32 -0400

    Added content to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,60 @@
+import "../styles/About.css";
+
 function About() {
-    return (
-        <div className="about">
-            <h1>About Us</h1>
-        </div>
-    )
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <h3>You spend the most time at home - Why not love where you live.</h3>
+      <div className="about-body">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          consequuntur laborum quidem laudantium iure odit! Totam non saepe
+          ipsum unde, maiores error esse, vitae itaque magnam dolorum dolore
+          officiis rem! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Cupiditate consequuntur laborum quidem laudantium iure odit! Totam non
+          saepe ipsum unde, maiores error esse, vitae itaque magnam dolorum
+          dolore officiis rem!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          dolore distinctio sed praesentium voluptate, minus facere enim nam
+          quaerat magni ullam numquam quisquam earum necessitatibus suscipit
+          expedita id error natus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Cupiditate consequuntur laborum quidem laudantium
+          iure odit! Totam non saepe ipsum unde, maiores error esse, vitae
+          itaque magnam dolorum dolore officiis rem!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          similique libero sapiente provident incidunt odio corrupti quaerat,
+          praesentium perferendis autem quibusdam soluta voluptatibus officia
+          rem repellendus vero at. Optio, laboriosam. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Cupiditate consequuntur laborum quidem
+          laudantium iure odit! Totam non saepe ipsum unde, maiores error esse,
+          vitae itaque magnam dolorum dolore officiis rem!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum magnam
+          qui cum amet debitis quasi quis fugit ratione sit fugiat molestiae
+          culpa, expedita veniam beatae dolores dicta odit ab excepturi! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          consequuntur laborum quidem laudantium iure odit! Totam non saepe
+          ipsum unde, maiores error esse, vitae itaque magnam dolorum dolore
+          officiis rem!
+        </p>
+      </div>
+      <div className="testimonials">
+        <h2>What our clients say</h2>
+        <p className="testimonials-body">
+         " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim sequi
+          illo illum veniam error, voluptatum repudiandae corrupti, dolor aut
+          dignissimos dolorum et, nemo totam. Iusto harum fugit quo sint
+          tempore? "
+        </p>
+        <h3 className="testimonial-author">Jane Doe</h3>
+      </div>
+    </div>
+  );
 }
 
 export default About;

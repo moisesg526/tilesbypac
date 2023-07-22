b311676053f34445a8898353db76ee075944014a
moises gomez <moisesg526@yahoo.com>
2023-07-21 22:46:08 -0400

    Added a few more images to gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -19,6 +19,20 @@ function Gallery() {
             alt="modern bathroom"
             className="gallery-img"
           />
+        </div>{" "}
+        <div className="gallery-img-container">
+          <img
+            src={require("../images/glass-bathroom.jpg")}
+            alt="modern bathroom"
+            className="gallery-img"
+          />
+        </div>
+        <div className="gallery-img-container">
+          <img
+            src={require("../images/darkglass-bathroom.jpg")}
+            alt="modern bathroom"
+            className="gallery-img"
+          />
         </div>
         <div className="gallery-img-container">
           <img
@@ -33,6 +47,13 @@ function Gallery() {
         <div className="gallery-img-container">
           <img
             src={require("../images/whitekitchen.jpg")}
+            alt="white kitchen"
+            className="gallery-img"
+          />
+        </div>
+        <div className="gallery-img-container">
+          <img
+            src={require("../images/clean-brick-kitchen.jpg")}
             alt="white kitchen"
             className="gallery-img"
           />

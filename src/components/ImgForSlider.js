71e028b5a40bd12f2ebf35b651683711817fdb25
moises gomez <moisesg526@yahoo.com>
2023-08-05 20:37:33 -0400

    Added a file for images to be called in gallery page

---
+++ src/components/ImgForSlider.js
@@ -0,0 +1,69 @@
+const ImgForSlider = [
+    {
+        id:1,
+        type: "bathroom",
+        img: "../images/bathroomdark.jpg"
+
+    },
+    {
+        id:2,
+        type: "bathroom",
+        img: "../images/bathroommodern.jpg"
+
+    },
+        {
+        id:3,
+        type: "bathroom",
+        img: "../images/glass-bathroom.jpg"
+
+    },
+    {
+        id:4,
+        type: "bathroom",
+        img: "../images/darkglass-bathroom.jpg"
+
+    },
+    {
+        id:5,
+        type: "bathroom",
+        img: "../images/bathroomsink.jpg"
+
+    },
+    {
+        id:6,
+        type: "kitchen",
+        img: "../images/whitekitchen.jpg"
+
+    },
+    {
+        id:7,
+        type: "kitchen",
+        img: "../images/clean-brick-kitchen.jpg"
+
+    },
+    {
+        id:8,
+        type: "kitchen",
+        img: "../images/kitchentiles.jpg"
+
+    },
+    {
+        id:9,
+        type: "kitchen",
+        img: "../images/kitchensink.jpg"
+
+    },
+    {
+        id:10,
+        type: "kitchen",
+        img: "../images/lemonkitchen.jpg"
+
+    },
+    {
+        id:11,
+        type: "kitchen",
+        img: "../images/finishedkitchen.jpg"
+
+    }
+]
+

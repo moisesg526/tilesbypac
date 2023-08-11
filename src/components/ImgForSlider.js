e7cf7db94c7469f0ff3370bc53a59fb327a328f4
moises gomez <moisesg526@yahoo.com>
2023-08-10 22:33:25 -0400

    Added react-modal npm

--- src/components/ImgForSlider.js
+++ src/components/ImgForSlider.js
@@ -1,70 +1,59 @@
 const ImgForSlider = [
-    {
-        id:1,
-        type: "bathroom",
-        img: "../images/bathroomdark.jpg"
-
-    },
-    {
-        id:2,
-        type: "bathroom",
-        img: "../images/bathroommodern.jpg"
-
-    },
-        {
-        id:3,
-        type: "bathroom",
-        img: "../images/glass-bathroom.jpg"
-
-    },
-    {
-        id:4,
-        type: "bathroom",
-        img: "../images/darkglass-bathroom.jpg"
-
-    },
-    {
-        id:5,
-        type: "bathroom",
-        img: "../images/bathroomsink.jpg"
-
-    },
-    {
-        id:6,
-        type: "kitchen",
-        img: "../images/whitekitchen.jpg"
-
-    },
-    {
-        id:7,
-        type: "kitchen",
-        img: "../images/clean-brick-kitchen.jpg"
-
-    },
-    {
-        id:8,
-        type: "kitchen",
-        img: "../images/kitchentiles.jpg"
-
-    },
-    {
-        id:9,
-        type: "kitchen",
-        img: "../images/kitchensink.jpg"
-
-    },
-    {
-        id:10,
-        type: "kitchen",
-        img: "../images/lemonkitchen.jpg"
-
-    },
-    {
-        id:11,
-        type: "kitchen",
-        img: "../images/finishedkitchen.jpg"
-
-    }
-]
+  {
+    type: "image",
+    room: "bathroom",
+    img: "../images/bathroomdark.jpg",
+  },
+  {
+    type: "image",
+    room: "bathroom",
+    img: "../images/bathroommodern.jpg",
+  },
+  {
+    type: "image",
+    room: "bathroom",
+    img: "../images/glass-bathroom.jpg",
+  },
+  {
+    type: "image",
+    room: "bathroom",
+    img: "../images/darkglass-bathroom.jpg",
+  },
+  {
+    type: "image",
+    room: "bathroom",
+    img: "../images/bathroomsink.jpg",
+  },
+  {
+    type: "image",
+    room: "kitchen",
+    img: "../images/whitekitchen.jpg",
+  },
+  {
+    type: "image",
+    room: "kitchen",
+    img: "../images/clean-brick-kitchen.jpg",
+  },
+  {
+    type: "image",
+    room: "kitchen",
+    img: "../images/kitchentiles.jpg",
+  },
+  {
+    type: "image",
+    room: "kitchen",
+    img: "../images/kitchensink.jpg",
+  },
+  {
+    type: "image",
+    room: "kitchen",
+    img: "../images/lemonkitchen.jpg",
+  },
+  {
+    type: "image",
+    room: "kitchen",
+    img: "../images/finishedkitchen.jpg",
+  },
+];
 
 export default ImgForSlider;
